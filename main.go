28a992dccc67a9858273b55b3c0fa51de646c85b
gloomysw <sw.gloomysw@gmail.com>
2016-06-29 16:20:21 +0800

    极智汇匹配日志清单处理程序

---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"bufio"
+	"io"
+	"strings"
+)
+
+var (
+	fileArray []string
+	phoneCount map[string]int=make(map[string]int)
+	urlMatch map[string]int=make(map[string]int)
+)
+
+func main() {
+
+	//sdfsdf:=strings.Split("128869346390430464      1467090830      275","      ")
+	//fmt.Println(sdfsdf[1])
+	readFile("./jizhihui.log.2016-06-28-00")
+
+	var phone []string
+
+	for _, value:=range fileArray  {
+		phone=strings.Split(value,"	")
+		phoneCount[phone[0]]=0
+		_, ok := urlMatch[phone[2]]
+		if !ok {
+			urlMatch[phone[2]]=1
+			continue
+		}
+		urlMatch[phone[2]]++
+	}
+	fmt.Println(len(phoneCount))
+
+	fmt.Printf("%v",urlMatch)
+}
+
+/**
+逐行读取文件
+创建人:邵炜
+创建时间:2016年6月29日15:41:51
+输入参数:文件路劲
+ */
+func readFile(filePath string) {
+	var(
+		readAll =false
+		readByte []byte
+		line []byte
+	)
+	read,err:=os.Open(filePath)
+	if err != nil {
+		fmt.Printf("文件打开失败! err: %s \n",err.Error())
+		return
+	}
+	defer read.Close()
+	buf:=bufio.NewReader(read)
+	for err!=io.EOF {
+		if err != nil {
+			fmt.Printf("文件读取错误! err: %s \n",err.Error())
+		}
+		if readAll {
+			readByte,readAll,err=buf.ReadLine()
+			line=append(line,readByte...)
+		}else{
+			readByte,readAll,err=buf.ReadLine()
+			line=append(line,readByte...)
+			if len(strings.TrimSpace(string(line)))==0 {
+				continue
+			}
+			fileArray=append(fileArray,string(line))
+			line=line[:0]
+		}
+	}
+}
